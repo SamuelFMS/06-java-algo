f6b4f695f18742d26660965bcd3afc4d906356ab
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-06 09:49:01 +0200

    Ajout de commentaire

--- Jeu du pendu/src/Main.java
+++ Jeu du pendu/src/Main.java
@@ -8,10 +8,24 @@ public class Main {
     public static final String LIGHT_GRAY = "\u001B[38;5;250m";
     public static final String RESET = "\u001B[0m";
 
+    /**
+     * Number of try left
+     */
     protected static int numberOfTry = 6;
+    /**
+     * List of word to guess
+     */
     protected static String[] listOFWords = {"ACCORD", "ACTEUR", "AGENDA", "AGNEAU", "ALBUM", "ALERTE", "AMANDE", "AMICAL", "ANIMAL", "ANANAS", "ARBRE", "ARDOISE", "ARGENT", "ARMOIRE", "ASTUCE", "AVION", "AVOCAT", "BALLE", "BANANE", "BARQUE", "BASSIN", "BATEAU", "BICYCLETTE", "BIJOU", "BILLET", "BOCAL", "BOUGIE", "BOUTEILLE", "BOUTON", "BRANCHE", "BUREAU", "CABANE", "CADEAU", "CAHIER", "CAILLOU", "CALCUL", "CAMERA", "CAMION", "CANARD", "CANYON", "CARNET", "CARTON", "CASQUE", "CERISE", "CHAISE", "CHALEUR", "CHAPEAU", "CHASSE", "CHATON", "CHAUSSURE", "CHEMISE", "CHEVAL", "CHEVEU", "CHIEN", "CHIFFRE", "CIMENT", "CINEMA", "CISEAUX", "CITRON", "CLASSE", "CLAVIER", "CLOCHARD", "COFFRE", "COLLE", "COLORE", "COMPTE", "COPAIN", "CORDE", "COULOIR", "COUTEAU", "CRAIE", "CRAYON", "CUISINE", "CUIVRE", "DANSEUR", "DAUPHIN", "DECOR", "DEFENSE", "DESSIN", "DEVINETTE", "DICTEE", "DIGITAL", "DINOSAURE", "DOCTEUR", "DOMINO", "DRAPEAU", "DROITE", "ECLAIR", "ECOLE", "ECRAN", "ECUREUIL", "EFFORT", "ENFANT", "ENIGME", "EPAULE", "EPONGE", "EQUIPE", "ESPACE", "ESPRIT", "ETOILE", "FACTORIE", "FAMILLE", "FANTOME", "FARINE", "FAUTEUIL", "FENETRE", "FERMIER", "FEUILLE", "FIDELE", "FILLETTE", "FLEUVE", "FLOCON", "FORGEUR", "FORMAT", "FRAISE", "FROMAGE", "FUSIBLE", "GARAGE", "GARCON", "GATEAU", "GAUCHE", "GAZON", "GIRAFE", "GLACE", "GOMME", "GOUDRON", "GRANDEUR", "GRENOUILLE", "GRIFFE", "GUITARE", "HABIT", "HAZARD", "HIBOU", "HISTOIRE", "HORLOGE", "HOTEL", "HUMAIN", "HUMOUR", "IMAGE", "INSECTE", "INVITE", "IVOIRE", "JARDIN", "JAUNE", "JOURNAL", "JUNGLE", "KANGOUROU", "KLAXON", "LABORATOIRE", "LAMPE"};
-    protected static ArrayList<Character> listAttempedChar = new ArrayList<Character>();
+    /**
+     * List of already attempted char
+     */
+    protected static ArrayList<Character> listAttemptedChar = new ArrayList<Character>();
 
+    /**
+     * Return a char from a to z that the user didnt already input
+     * @param scanner
+     * @return
+     */
     public static char inputLetter(Scanner scanner) {
         char result = '\0'; // Empty char (Unicode 0)
 
@@ -21,7 +35,7 @@ public class Main {
 
             if (nextString.length() == 1) {
                 if (nextString.charAt(0) >= 'A' && nextString.charAt(0) <= 'Z') {
-                    if (!listAttempedChar.contains(nextString.charAt(0))) {
+                    if (!listAttemptedChar.contains(nextString.charAt(0))) {
                         result = nextString.charAt(0);
                     } else {
                         System.out.println("Vous avez déjà essayer " + nextString.charAt(0) + "!");
@@ -36,10 +50,22 @@ public class Main {
         return result;
     }
 
+    /**
+     * Return a random word from the list
+     * @param random
+     * @return
+     */
     public static String randomWord(Random random) {
         return listOFWords[random.nextInt(listOFWords.length)];
     }
 
+    /**
+     * Return the String with the char if the word contains otherwise reduce the number of try
+     * @param wordToFind
+     * @param hideString
+     * @param a
+     * @return
+     */
     public static String findCharInString(String wordToFind, String hideString, char a) {
         StringBuilder newString = new StringBuilder(hideString);
         if (wordToFind.contains(String.valueOf(a))) {
@@ -56,17 +82,31 @@ public class Main {
         return String.valueOf(newString);
     }
 
+    /**
+     * Function that return the string but with _
+     * @param wordToHide
+     * @return
+     */
     public static String hideString(String wordToHide) {
         return wordToHide.replaceAll(".", "_");
     }
 
+    /**
+     * Verify if the game is finish
+     * @param wordToFind
+     * @param hiddenWord
+     * @return
+     */
     public static boolean isGameFinished(String wordToFind, String hiddenWord) {
         return wordToFind.equalsIgnoreCase(hiddenWord) || numberOfTry <= 0;
     }
 
+    /**
+     * Display all the character that the user didn't input and put it in black for the char already used
+     */
     public static void displayRemainingLetter() {
         for (int charCode = 'A'; charCode <= 'Z'; charCode++) {
-            if (!listAttempedChar.contains((char) charCode)) {
+            if (!listAttemptedChar.contains((char) charCode)) {
                 System.out.print(LIGHT_GRAY + (char) charCode + " " + RESET);
             } else {
                 System.out.print((char) charCode + " ");
@@ -76,6 +116,10 @@ public class Main {
         System.out.println();
     }
 
+    /**
+     * Main method
+     * @param args
+     */
     public static void main(String[] args) {
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
@@ -86,7 +130,7 @@ public class Main {
             System.out.println("Mot Mystère :" + hiddenWord);
             displayRemainingLetter();
             char inputChar = inputLetter(scanner);
-            listAttempedChar.add(inputChar);
+            listAttemptedChar.add(inputChar);
             hiddenWord = findCharInString(wordToFind, hiddenWord, inputChar);
         }
 
